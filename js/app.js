2098a5b4ab7e39bab3307b8594c1dd3ff3cc1f23
kbarne12 <kbarne12@gmail.com>
2021-03-06 17:39:26 -0500

    going still

--- js/app.js
+++ js/app.js
@@ -6,28 +6,41 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let player = 1
-let dealer = -1
-let gameState = {
-    
-}
-
+let playerHand = []
+let dealerHand = []
+let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+let dealBtn = document.getElementById('btn')
+let playerHandEl = document.getElementById('playerHand')
+let dealerHandEl = document.getElementById('dealerHand')
+let stayBtn = document.getElementById('stay')
+let hitBtn = document.getElementById('hit')
 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+dealBtn.addEventListener('click', dealClick)
 
 
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-getWinner();
+function dealClick(){
+    
+    // pull 2 random cards to a variable
+    // splice the cards out of deck
+    // add card to player deck
+    // call render function
+}
+function render(pickedCards){
+    //remove outline
+    //add card css selector to player hand
+
+}
