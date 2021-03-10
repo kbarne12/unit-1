39370623b9b6d571a4bb57ac79732358a9fa007e
kbarne12 <kbarne12@gmail.com>
2021-03-09 23:18:54 -0500

    restart

--- js/app.js
+++ js/app.js
@@ -1,114 +1,65 @@
-/*-------------------------------- Constants --------------------------------*/
-
-
-
-
-
-
-/*
--------------------------------- Variables --------------------------------*/
-let dealer;
-let player;
-let playerHand = []
-let playerHit = []
-let dealerHand = []
-let isWinner;
-let calculate; 
-let stay = false
-let hitCard
-let turn = false
-let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
-let = "dA","dQ","dK","dJ","d10", "hA","hQ","hK","hJ","h10"
-let pickedCards
-let pickedCardR
-let pickedCardD
-let pickedCardD2
-let randIdx = Math.floor(Math.random()*deck.length)
-let randIdx2 = Math.floor(Math.random()*deck.length)
-let randIdx3 = Math.floor(Math.random()*deck.length)
-let randIdx4 = Math.floor(Math.random()*deck.length)
-pickedCards = deck.splice(randIdx, 1)
-pickedCardR = deck.splice(randIdx2, 1)
-pickedCardD = deck.splice(randIdx3, 1)
-pickedCardD2 = deck.splice(randIdx4, 1)
-playerHand.push(pickedCards, pickedCardR)
-dealerHand.push(pickedCardD, pickedCardD2)
-player = pickedCards + pickedCardR 
-    console.log('playerHand', playerHand)
-    console.log('dealerHand', dealerHand)
-    console.log(player)
-
-
-
-
+//Constances--------------------------------------------------------------------
+const cardVal = {
+    "dK": 10, "dQ": 10, "dJ": 10,"d10": 10,"d09": 9,"d08": 8,"d07": 7,"d06": 6,"d05": 5,"d04": 4,"d03": 3,"d02": 2,"hA": 0,"hQ": 10,"hK": 10,"hJ": 10,"h10": 10,"h09": 9,"h08": 8,"h07": 7,"h06": 6,"h05": 5,"h04": 4,"h03": 3,"h02": 2,"cA": 0,"cQ": 10,"cK": 10,"cJ": 10,"c10": 10,"c09": 9,"c08": 8,"c07": 7,"c06": 6,"c05": 5,"c04": 4,"c03": 3,"c02": 2,"sA": 0,"sQ": 10,"sK": 10,"sJ": 10,"s10": 10,"s09": 9,"s08": 8,"s07": 7,"s06": 6,"s05": 5,"s04": 4,"s03": 3,"s02": 2
+ 
+ }
+ const state = {
+    deck: ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"],
+    isFirstHand: true,
+    dealerHand: [],
+    playerHand: [],
+    stay: false,
+    bust: false,
+    hitCard: [],
+    drawnCard: []
+ }
+//variables-----------------------------------------------------------------------
+let currentDeck = state.deck;
 /*------------------------ Cached Element References ------------------------*/
-let dealBtn = document.getElementById('btn')
-let playerHandEl = document.getElementById('playerHand')
-let playerHandEl2 = document.getElementById('playerHand2')
-let playerHitEl = document.getElementById('playerHit')
-let playerHitEl2 = document.getElementById('playerHit2')
-let playerHitEl3 = document.getElementById('playerHit3')
-let dealerHandEl = document.getElementById('dealerHand')
-let dealerHandEl2 = document.getElementById('dealerHand2')
-let stayBtn = document.getElementById('stay')
-let hitBtn = document.getElementById('hit')
+    let dealBtn = document.getElementById("btn")
+// let stayBtn = document.getElementById('stay')
+// let hitBtn = document.getElementById('hit')
+// let playerHandEl = document.getElementById('playerHand')
+// let playerHandEl2 = document.getElementById('playerHand2')
+// let playerHitEl = document.getElementById('playerHit')
+// let playerHitEl2 = document.getElementById('playerHit2')
+// let playerHitEl3 = document.getElementById('playerHit3')
+// let dealerHandEl = document.getElementById('dealerHand')
+// let dealerHandEl2 = document.getElementById('dealerHand2')
+
 
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-dealBtn.addEventListener('click', dealClick)
-hitBtn.addEventListener('click', hitClick)
-stayBtn.addEventListener('click', stayFunc)
+dealBtn.addEventListener('click', dealCards)
+// hitBtn.addEventListener('click', hitClick)
+// stayBtn.addEventListener('click', stayFunc)
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-function stayFunc(){
-    turn = true
-
+function dealCards() {
+    if(state.isFirstHand){
+        shuffleArray(currentDeck);
+        state.isFirstHand = false;
+    }console.log(currentDeck)
 }
-// function to total points
-
-
-function dealClick(){
-    
-    render(pickedCards, pickedCardR, pickedCardD, pickedCardD2)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
-function render(pickedCards, pickedCardR, pickedCardD, pickedCardD2){
-    if(playerHand.length === 2 && dealerHand.length === 2){
-        
+
+function dealToPlayer() {
+    for (let i=0; i<2; i++) {
+        drawnCard = currentDeck.pop();
+        state.playerHand.push(drawnCard);
     }
-    playerHandEl.classList.add(pickedCards)
-    playerHandEl2.classList.add('card', pickedCardR)
-    dealerHandEl.classList.add('card', pickedCardD)
-    dealerHandEl2.classList.add('card', 'back-blue', pickedCardD2)
-       
-}    
-function hitClick(){
-    let randIdxHit = Math.floor(Math.random()*deck.length)
-    hitCard = deck.splice(randIdxHit, 4)
-    playerHit.push(hitCard)
-    console.log('playerHit', playerHit)
-    addHit(hitCard)
 }
-function addHit(hitCard){
-    if(playerHit.length === 1 && playerHand.length === 2){
-        playerHitEl.classList.remove('outline')
-        
-    }
-    playerHitEl.classList.add(hitCard[0])
-    if(playerHit.length === 2){
-        playerHitEl2.classList.remove('outline')
-        playerHitEl2.classList.add(hitCard[1])
-    }
-    if(playerHit.length === 3){
-        playerHitEl3.classList.remove('outline')
-        playerHitEl3.classList.add(hitCard[2])
-     }
-    }
 
 
 
